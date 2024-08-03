de0b2a166eeee1c5400fe8a6d6e53877e5261a30
WebRizen <webrizen@gmail.com>
2024-08-03 16:14:28 +0530

    "Updated logo image and styling in Navbar component"

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,17 +10,17 @@ const Navbar = () => {
     <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
         <div className="relative">
-          {/* <Image
-            src="/horseLogo.jpg"
+          <Image
+            src="/logo-white.png"
             alt="logo"
             width={40}
             height={40}
-            className="w-full h-full object-contain rounded-full"
-          /> */}
+            className="w-full md:h-20 h-10 object-contain rounded-full"
+          />
         </div>
-        <h1 className={`${DancingScript.className} text-white md:text-5xl text-2xl font-bold`}>
+        {/* <h1 className={`${DancingScript.className} text-white md:text-5xl text-2xl font-bold`}>
           Ammar Kheder
-        </h1>
+        </h1> */}
       </div>
 
       <div className="flex flex-row gap-5 mb-2">
